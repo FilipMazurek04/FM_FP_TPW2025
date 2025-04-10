09cd1d59ed63fe715b13d7d65d5c46e367398b0a
251609 Pecyna Filip <251609@edu.p.lodz.pl>
2025-04-10 17:04:52 +0200

    constrained balls to stay inside the table

--- BusinessLogic/BusinessBall.cs
+++ BusinessLogic/BusinessBall.cs
@@ -10,26 +10,69 @@
 
 namespace TP.ConcurrentProgramming.BusinessLogic
 {
-  internal class Ball : IBall
-  {
-    public Ball(Data.IBall ball)
+    internal class Ball : IBall
     {
-      ball.NewPositionNotification += RaisePositionChangeEvent;
+        private readonly Data.IBall _ball;
+        private static readonly double _ballDiameter = BusinessLogicAbstractAPI.GetDimensions.BallDimension;
+        private static readonly double _tableWidth = BusinessLogicAbstractAPI.GetDimensions.TableWidth;
+        private static readonly double _tableHeight = BusinessLogicAbstractAPI.GetDimensions.TableHeight;
+
+        public Ball(Data.IBall ball)
+        {
+            _ball = ball;
+            ball.NewPositionNotification += RaisePositionChangeEvent;
+        }
+
+        #region IBall
+
+        public event EventHandler<IPosition>? NewPositionNotification;
+
+        #endregion IBall
+
+        #region private
+
+        private void RaisePositionChangeEvent(object? sender, Data.IVector e)
+        {
+            // Ograniczamy współrzędne, żeby nie wyszły poza stół
+            double constrainedX = ConstrainPosition(e.x, _ballDiameter, _tableWidth);
+            double constrainedY = ConstrainPosition(e.y, _ballDiameter, _tableHeight);
+
+            // Odwracamy prędkość piłki jeśli uderzy w stół
+            if (constrainedX != e.x || constrainedY != e.y)
+            {
+                Data.IVector currentVelocity = _ball.Velocity;
+
+                if (constrainedX != e.x)
+                {
+                    _ball.Velocity = Data.DataAbstractAPI.GetDataLayer().CreateVector(-currentVelocity.x, currentVelocity.y);
+                }
+
+                if (constrainedY != e.y)
+                {
+                    _ball.Velocity = Data.DataAbstractAPI.GetDataLayer().CreateVector(currentVelocity.x, -currentVelocity.y);
+                }
+            }
+
+            NewPositionNotification?.Invoke(this, new Position(constrainedX, constrainedY));
+        }
+
+        private static double ConstrainPosition(double position, double ballSize, double tableSize)
+        {
+            double radius = ballSize / 2.0;
+            double min = radius;
+            double max = tableSize - radius;
+
+            if (position < min)
+            {
+                return min;
+            }
+            if (position > max)
+            {
+                return max;
+            }
+            return position;
+        }
+
+        #endregion private
     }
-
-    #region IBall
-
-    public event EventHandler<IPosition>? NewPositionNotification;
-
-    #endregion IBall
-
-    #region private
-
-    private void RaisePositionChangeEvent(object? sender, Data.IVector e)
-    {
-      NewPositionNotification?.Invoke(this, new Position(e.x, e.y));
-    }
-
-    #endregion private
-  }
 }
